f640f6b503ceff26d53540c08556375b1d337547
hapijul-trigent <hapijul_h@trigent.com>
2024-08-08 01:46:38 +0000

    untested update

--- app/main.py
+++ app/main.py
@@ -19,12 +19,26 @@ st.set_page_config(
 # Application
 st.image('static/images/Trigent_Logo_full.png')
 st.title("Clinical Trials NER Application")
+
+# SideBar
 uploaded_file = upload_file()
 if uploaded_file:
     text = extract_text(uploaded_file)
+    # Title and description
+    # st.sidebar.image("https://path-to-john-snow-labs-logo.png", width=200)
+    st.markdown('<p class="big-font">Detect Clinical Entities</p>', unsafe_allow_html=True)
+    st.write("Choose the pretrained model to test")
+    # Model selection
+    model = st.selectbox("Choose Pretrainde Model",
+        [
+        'ner_jsl','ner_jsl_slim'
+        'ner_jsl_enriched'
+        'ner_jsl_greedy'
+        ]
+    )
     if text.strip():
         # Your text area widget
-        st.text_area(label='Editor', value=text, height=int(len(text.strip())/2))
+        st.text_area(label='Editor', value=text, height=int(len(text.strip())/2.3))
         generateButton = st.button('Generate', type='primary')
     else:
         st.info('Empty File!')

40ddd2fd7f0ce8a88f6db8402c1d32d6387bb2bf
hapijul-trigent <hapijul_h@trigent.com>
2024-08-08 12:46:03 +0000

    Exception: Java gateway process exited before sending its port number

--- models/run.py
+++ models/run.py
@@ -1,5 +1,16 @@
-from model_setup import setup_config, get_spark
-# from ner_pipeline import getNerPipeline
+import os 
+import json
+
+with open('.secret/spark_nlp_for_healthcare_8568.json') as f:
+    license_keys = json.load(f)
+
+# Defining license key-value pairs as local variables
+locals().update(license_keys)
+os.environ.update(license_keys)
+
+
+
+# ------------------------------------------
 import sparknlp
 import sparknlp_jsl
 
@@ -17,28 +28,72 @@ pd.set_option('display.max_colwidth', 200)
 
 import warnings
 warnings.filterwarnings('ignore')
-keys = setup_config()
-# spark = get_spark(secret=keys['SECRET'])
-# pipe = getNerPipeline(selectedModel='ner_jsl', 
-#                       selectedEntities=
-#                       ["Test", "Oncological", "Procedure", "Symptom", "Treatments", "Diabetes", "Drug", "Dosage",]
-#                     )
-# print(pipe)
-from sparknlp.pretrained import PretrainedPipeline
-import sparknlp
 
-# Start a Spark NLP session
-spark = sparknlp.start()
+params = {"spark.driver.memory":"10G", 
+          "spark.kryoserializer.buffer.max":"2000M", 
+          "spark.driver.maxResultSize":"2000M"} 
 
-# Load a pretrained medical NER pipeline
-pipeline = PretrainedPipeline('ner_jsl', 'en', 'clinical/models')
+spark = sparknlp_jsl.start(license_keys['SECRET'],params=params)
 
-# Sample text
-text = "The patient was diagnosed with diabetes and prescribed metformin."
+print("Spark NLP Version :", sparknlp.version())
+print("Spark NLP_JSL Version :", sparknlp_jsl.version())
 
-# Annotate the text
-annotations = pipeline.fullAnnotate(text)
+print(spark)
 
-# Print annotations
-for annotation in annotations[0]['ner_chunk']:
-    print(f"Entity: {annotation.result}, Type: {annotation.metadata['entity']}")
+#-------------------------------------
+sample_text = """The patient is a 40-year-old white male who presents with a chief complaint of "chest pain". The patient is diabetic and has a prior history of coronary artery disease. The patient presents today stating that his chest pain started yesterday evening and has been somewhat intermittent. He has been advised Aspirin 81 milligrams QDay, insulin 50 units in a.m. HCTZ 50 mg QDay. Nitroglycerin 1/150 sublingually PRN chest pain."""
+
+
+
+# ---------------------------------------------
+jsl_model_list = ["ner_jsl",]
+
+
+documentAssembler = DocumentAssembler()\
+                .setInputCol("text")\
+                .setOutputCol("document")
+
+sentenceDetector = SentenceDetectorDLModel.pretrained("sentence_detector_dl_healthcare","en","clinical/models") \
+                .setInputCols(["document"]) \
+                .setOutputCol("sentence") 
+
+tokenizer = Tokenizer()\
+                .setInputCols(["sentence"])\
+                .setOutputCol("token")
+
+jsl_ner_converter = NerConverterInternal() \
+                .setInputCols(["sentence", "token", "jsl_ner"]) \
+                .setOutputCol("ner_chunk")
+
+embeddings = WordEmbeddingsModel.pretrained("embeddings_clinical", "en", "clinical/models")\
+                .setInputCols(["sentence", "token"])\
+                .setOutputCol("embeddings")
+  
+for model_name in jsl_model_list:
+
+  jsl_ner = MedicalNerModel.pretrained(model_name, "en", "clinical/models") \
+                          .setInputCols(["sentence", "token", "embeddings"]) \
+                          .setOutputCol("jsl_ner")
+
+
+  jsl_ner_pipeline = Pipeline(stages=[documentAssembler, 
+                                      sentenceDetector,
+                                      tokenizer,
+                                      embeddings,
+                                      jsl_ner,
+                                      jsl_ner_converter])
+
+
+  jsl_ner_model = jsl_ner_pipeline.fit(spark.createDataFrame([['']]).toDF("text"))
+  
+  light_model = LightPipeline(jsl_ner_model)
+  light_result = light_model.fullAnnotate(sample_text)
+
+  print("\n\n\n")
+  print(f"***************  The visualization results for {model_name} ***************")
+  print("\n\n\n")
+
+  from sparknlp_display import NerVisualizer
+  visualiser = NerVisualizer()
+  visualiser.display(light_result[0], label_col='ner_chunk', document_col='document')
+  print("\n\n\n")
